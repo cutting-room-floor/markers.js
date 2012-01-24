3701c075ffe2a80b75138f1c1604d71abc08783b
Tom MacWright <tom@macwright.org>
2012-01-24 12:21:44 -0500

    IE compatibility, I hate you.

--- src/mmg.js
+++ src/mmg.js
@@ -1,7 +1,42 @@
+// Array.indexOf polyfill courtesy of Mozilla MDN:
+// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+        "use strict";
+        if (this === void 0 || this === null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (len === 0) {
+            return -1;
+        }
+        var n = 0;
+        if (arguments.length > 0) {
+            n = Number(arguments[1]);
+            if (n !== n) { // shortcut for verifying if it's NaN
+                n = 0;
+            } else if (n !== 0 && n !== Infinity && n !== -Infinity) {
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));
+            }
+        }
+        if (n >= len) {
+            return -1;
+        }
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+        for (; k < len; k++) {
+            if (k in t && t[k] === searchElement) {
+                return k;
+            }
+        }
+        return -1;
+    };
+}
+
 function mmg() {
     var l = {},
         // a list of our markers
-        markers = null,
+        markers = [],
         // the absolute position of the parent element
         position = null,
         factory = null,
@@ -12,8 +47,6 @@ function mmg() {
     var parent = document.createElement('div');
     parent.style.cssText = 'position: absolute; top: 0px;' +
       'left: 0px; width: 100%; height: 100%; margin: 0; padding: 0; z-index: 0';
-
-    markers = [];
 
     function defaultFactory(feature) {
         var d = document.createElement('div');
@@ -34,8 +67,7 @@ function mmg() {
     function repositionAllMarkers() {
         left = l.map.pointLocation(new MM.Point(0, 0));
         right = l.map.pointLocation(new MM.Point(l.map.dimensions.x, 0));
-        var len = markers.length;
-        for (var i = 0; i < len; i++) {
+        for (var i = 0; i < markers.length; i++) {
             repositionMarker(markers[i]);
         }
     }
@@ -47,11 +79,11 @@ function mmg() {
         var pos = l.map.coordinatePoint(marker.coord);
         var pos_loc;
         if (pos.x < 0) {
-            pos_loc = marker.location.copy();
+            pos_loc = new MM.Location(marker.location.lat, marker.location.lon);
             pos_loc.lon += Math.ceil((left.lon - marker.location.lon) / 360) * 360;
             pos = l.map.locationPoint(pos_loc);
         } else if (pos.x > l.map.dimensions.x) {
-            pos_loc = marker.location.copy();
+            pos_loc = new MM.Location(marker.location.lat, marker.location.lon);
             pos_loc.lon -= Math.ceil((marker.location.lon - right.lon) / 360) * 360;
             pos = l.map.locationPoint(pos_loc);
         }
@@ -68,6 +100,7 @@ function mmg() {
      * provided GeoJSON feature, Location instance (or {lat,lon} object
      * literal), or "lat,lon" string.
      */
+    var first = true;
     l.addMarker = function(marker, feature) {
         if (!marker || !feature) {
             return null;
@@ -83,6 +116,7 @@ function mmg() {
 
         // add it to the list
         markers.push(marker);
+
         return marker;
     };
 
@@ -129,6 +163,7 @@ function mmg() {
     l.parent = parent;
 
     l.factory(defaultFactory);
+
 
     return l;
 }
