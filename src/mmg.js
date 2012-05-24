1b16358dd0fa869616960822ba7ad734e78770be
Tom MacWright <tom@macwright.org>
2012-05-24 17:45:31 -0400

    Update. simplestyle

--- src/mmg.js
+++ src/mmg.js
@@ -1,8 +1,10 @@
 function mmg() {
 
     var mmg = {},
-        // a list of our markers
+        // external list of geojson features
         geojson_features = [],
+        // internal list of markers
+        markers = [],
         // the absolute position of the parent element
         position = null,
         factory = null,
@@ -10,71 +12,65 @@ function mmg() {
         left = null,
         right = null;
 
-    // Reposition al markers
-    function repositionAllMarkers() {
-        left = l.map.pointLocation(new MM.Point(0, 0));
-        right = l.map.pointLocation(new MM.Point(l.map.dimensions.x, 0));
-        for (var i = 0; i < markers.length; i++) {
-            repositionMarker(markers[i]);
-        }
-    }
-
     // reposition a single marker element
     function repositionMarker(marker) {
         // remember the tile coordinate so we don't have to reproject every time
-        if (!marker.coord) marker.coord = l.map.locationCoordinate(marker.location);
-        var pos = l.map.coordinatePoint(marker.coord);
+        if (!marker.coord) marker.coord = mmg.map.locationCoordinate(marker.location);
+        var pos = mmg.map.coordinatePoint(marker.coord);
         var pos_loc;
         if (pos.x < 0) {
             pos_loc = new MM.Location(marker.location.lat, marker.location.lon);
             pos_loc.lon += Math.ceil((left.lon - marker.location.lon) / 360) * 360;
-            pos = l.map.locationPoint(pos_loc);
-        } else if (pos.x > l.map.dimensions.x) {
+            pos = mmg.map.locationPoint(pos_loc);
+        } else if (pos.x > mmg.map.dimensions.x) {
             pos_loc = new MM.Location(marker.location.lat, marker.location.lon);
             pos_loc.lon -= Math.ceil((marker.location.lon - right.lon) / 360) * 360;
-            pos = l.map.locationPoint(pos_loc);
+            pos = mmg.map.locationPoint(pos_loc);
         }
         if (pos_loc) {
-            marker.coord = l.map.locationCoordinate(pos_loc);
+            marker.coord = mmg.map.locationCoordinate(pos_loc);
         }
         pos.scale = 1;
         pos.width = pos.height = 0;
         MM.moveElement(marker, pos);
     }
 
-    mmg.addMarker = function(marker, feature) {
-        if (!marker || !feature) {
-            return null;
+    mmg.draw = function() {
+        left = mmg.map.pointLocation(new MM.Point(0, 0));
+        right = mmg.map.pointLocation(new MM.Point(mmg.map.dimensions.x, 0));
+        for (var i = 0; i < markers.length; i++) {
+            repositionMarker(markers[i]);
         }
+    };
+
+    mmg.addMarker = function(marker, feature) {
+        if (!marker || !feature) return null;
         // convert the feature to a Location instance
         marker.location = new MM.Location(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
         // position: absolute
         marker.style.position = 'absolute';
         // update the marker's position
-        if (l.map) repositionMarker(marker);
+        if (mmg.map) repositionMarker(marker);
         // append it to the DOM
         parent.appendChild(marker);
-
         // add it to the list
         markers.push(marker);
-
         return marker;
     };
-
+    
+    // Public data interface
     mmg.geojson = function(x) {
         // Return features
-        if (!arguments.length) {
-            return {
-                type: 'FeatureCollection',
-                features: geojson_features
-            };
-        }
+        if (!arguments.length) return {
+            type: 'FeatureCollection',
+            features: geojson_features
+        };
 
         // Clear features
-        // removing lastChild iteratively is faster than
-        // innerHTML = ''
-        // http://jsperf.com/innerhtml-vs-removechild-yo/2
         while (parent.hasChildNodes()) {
+            // removing lastChild iteratively is faster than
+            // innerHTML = ''
+            // http://jsperf.com/innerhtml-vs-removechild-yo/2
             parent.removeChild(parent.lastChild);
         }
         geojson_features = [];
@@ -82,34 +78,30 @@ function mmg() {
         // Set features
         if (x && x.features) {
             for (var i = 0; i < x.features.length; i++) {
-                l.addMarker(factory(x.features[i]), x.features[i]);
+                mmg.addMarker(factory(x.features[i]), x.features[i]);
             }
         }
 
         return mmg;
     };
 
-    mmg.draw = function() {
-        repositionAllMarkers();
-    };
-
+    // Factory interface
     mmg.factory = function(x) {
         if (!x) return factory;
         factory = x;
         return mmg;
     };
-
-    var parent = document.createElement('div');
-    parent.style.cssText = 'position: absolute; top: 0px;' +
-        'left: 0px; width: 100%; height: 100%; margin: 0; padding: 0; z-index: 0';
-
-    mmg.parent = parent;
-
     mmg.factory(function defaultFactory(feature) {
         var d = document.createElement('div');
         d.className = 'mmg-default';
         return d;
     });
+
+    // The parent DOM element
+    var parent = document.createElement('div');
+    parent.style.cssText = 'position: absolute; top: 0px;' +
+        'left: 0px; width: 100%; height: 100%; margin: 0; padding: 0; z-index: 0';
+    mmg.parent = parent;
 
     return mmg;
 }
