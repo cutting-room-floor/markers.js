a1eb5d2a40f8ee51f11ee4d9940b09e7c84c0302
Tom MacWright <tom@macwright.org>
2012-05-31 14:18:41 -0400

    Add simplestyle_factory tests

---
+++ test/spec/simplestyle_factory.js
@@ -0,0 +1,34 @@
+describe('simplestyle factory', function() {
+  it('creates proper elements', function() {
+    var ft = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77, 37.8]
+      },
+      "properties": {
+        "title": "This is a bus",
+        "marker-shape": "pin",
+        "marker-size": "medium",
+        "marker-symbol": "bus",
+        "marker-color": "#1ae"
+      }
+    };
+    var elem = simplestyle_factory(ft);
+    expect(jasmine.isDomNode(elem)).toBeTruthy();
+  });
+  it('does not fail when elements do not have style info', function() {
+    var ft = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77, 37.8]
+      },
+      "properties": {
+        "title": "This is a bus"
+      }
+    };
+    var elem = simplestyle_factory(ft);
+    expect(jasmine.isDomNode(elem)).toBeTruthy();
+  });
+});
