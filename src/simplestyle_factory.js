adf60f87469538f1cc8c1371b893b5d863ac3dab
Tom MacWright <tom@macwright.org>
2012-05-31 13:25:22 -0400

    Fix textindent

--- src/simplestyle_factory.js
+++ src/simplestyle_factory.js
@@ -22,8 +22,8 @@ function simplestyle_factory(feature) {
     ds.backgroundImage = 'url(http://a.tiles.mapbox.com/v3/marker/' +
       'pin-' + size[0] + symbol + '+' + color + '.png)';
 
-    ds.textIndent = -1e8;
-    d.innerHTML = fp.name || '';
+    ds.textIndent = -1e8 + 'px';
+    d.innerHTML = fp.title || '';
 
     return d;
 
