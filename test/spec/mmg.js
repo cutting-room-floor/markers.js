ae469d7892f38403ba1c9a7b8755bacc5c44cc51
Tom MacWright <tom@macwright.org>
2012-06-29 12:50:28 -0400

    Fix click issue by updating modestmaps. Fixes #16

--- test/spec/mmg.js
+++ test/spec/mmg.js
@@ -18,10 +18,7 @@ describe('mmg', function() {
 
     it('the default factory creates a dom node', function() {
       var m = mmg();
-      var foo = function() {};
       expect(jasmine.isDomNode(m.factory()()));
-      expect(m.factory(foo)).toEqual(m);
-      expect(m.factory()).toEqual(foo);
     });
 
     it('can be assigned a new factory function', function() {
