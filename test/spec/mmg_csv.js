51dbeaa2ef3f3b06d703de1e2cebbacf1ee0c65e
Tom MacWright <tom@macwright.org>
2012-07-08 19:31:08 -0400

    Test for property parsing.

--- test/spec/mmg_csv.js
+++ test/spec/mmg_csv.js
@@ -3,4 +3,9 @@ describe('mmg csv', function() {
         var csv = 'lat,lon\n10,15';
         expect(mmg_csv(csv)[0].geometry.coordinates).toEqual([15, 10]);
     });
+
+    it('can parse the properties of a csv file csv file', function() {
+        var csv = 'lat,lon,name\n10,15,Tom';
+        expect(mmg_csv(csv)[0].properties.name).toEqual('Tom');
+    });
 });
