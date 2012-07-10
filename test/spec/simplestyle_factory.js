4a7f8b3e9bc36c2c7c04b180e8936f1e9a0f721e
Tom MacWright <tom@macwright.org>
2012-07-10 10:18:58 -0400

    Test for the bug fix in 0.5.1 - that images should have the correct URL.

--- test/spec/simplestyle_factory.js
+++ test/spec/simplestyle_factory.js
@@ -17,6 +17,24 @@ describe('simplestyle factory', function() {
         var elem = simplestyle_factory(ft);
         expect(jasmine.isDomNode(elem)).toBeTruthy();
     });
+    it('generates a proper url for an icon', function() {
+        var ft = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-77, 37.8]
+            },
+            "properties": {
+                "title": "This is a bus",
+                "marker-shape": "pin",
+                "marker-size": "medium",
+                "marker-symbol": "bus",
+                "marker-color": "#1ae"
+            }
+        };
+        var elem = simplestyle_factory(ft);
+        expect(elem.src).toEqual('http://a.tiles.mapbox.com/v3/marker/pin-m-bus+1ae.png');
+    });
     it('does not fail when elements do not have style info', function() {
         var ft = {
             "type": "Feature",
